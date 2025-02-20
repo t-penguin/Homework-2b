100b5750cba10756e4648e23c8e094240c853179
Joseph <sourpina@gmail.com>
2025-02-20 00:15:57 -0500

    Added pseudocode for program flow

--- Part-5-Guided-Project/Starter/Program.cs
+++ Part-5-Guided-Project/Starter/Program.cs
@@ -6,3 +6,15 @@ string[] pettingZoo =
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
+
+// Randomize the pettings zoo array
+//RandomizeAnimals();
+
+// Assign a randomized animal set to a subset group
+//string[,] group = AssignGroup();
+
+// Print the school name
+Console.WriteLine("School A");
+
+// Print the animal groups
+//PrintGroup(group);
